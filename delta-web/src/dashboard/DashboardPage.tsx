4914be82b7e1788988508785c114389a04d36e95
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-14 20:53:16 +0900

    최소한의 대시보드 페이지를 추가합니다

---
+++ delta-web/src/dashboard/DashboardPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from '../core/Container';
+
+const DashboardPage = () => {
+  return <Container>
+    <h2>대시보드 페이지</h2>
+  </Container>;
+};
+
+export default DashboardPage;
