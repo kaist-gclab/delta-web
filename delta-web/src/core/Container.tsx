a7e9be2c6ce8ed36f946f4d970a8ed00f70edff9
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-18 07:08:49 +0900

    공통으로 사용되는 레이아웃 요소 Container와 Loading의 스타일을 개선합니다

--- delta-web/src/core/Container.tsx
+++ delta-web/src/core/Container.tsx
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.div`
   margin-left: 20px;
   margin-right: 20px;
+  margin-bottom: 40px;
 `;
--- delta-web/src/core/Loading.tsx
+++ delta-web/src/core/Loading.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-margin-top: 40px;
-`;
 
 export const Loading: React.FC = () => {
-  return <Container>
-    <div className="bp3-non-ideal-state">
-      <div className="bp3-non-ideal-state-visual">
-        <span className="bp3-icon bp3-icon-refresh"></span>
-      </div>
-      <h4 className="bp3-heading">불러오는 중</h4>
-      <div>잠시만 기다려 주세요.</div>
+  return <div className="bp3-non-ideal-state">
+    <div className="bp3-non-ideal-state-visual">
+      <span className="bp3-icon bp3-icon-refresh"></span>
     </div>
-  </Container>;
+    <h4 className="bp3-heading">불러오는 중</h4>
+    <div>잠시만 기다려 주세요.</div>
+  </div>;
 };
