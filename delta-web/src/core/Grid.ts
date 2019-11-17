065a53cbef19985c19e6c18f4308bdb245085fc7
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-18 07:07:35 +0900

    레이아웃을 지원하기 위한 Grid를 추가합니다

---
+++ delta-web/src/core/Grid.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  width: 100%;
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  flex: 1;
+`;
