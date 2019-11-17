76f55c2a5a4715d46f600dd7cc86393ce4a12024
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-18 03:45:33 +0900

    배포 후에는 이미지 파일을 제거하도록 합니다

--- delta-web/remote.sh
+++ delta-web/remote.sh
@@ -2,4 +2,5 @@ sudo docker load --input docker-image.tar && \
 (sudo docker stop delta-web || true) && \
 (sudo docker rm delta-web || true) && \
 sudo docker run -d --restart=unless-stopped \
---name delta-web delta-web
+--name delta-web delta-web && \
+rm docker-image.tar
