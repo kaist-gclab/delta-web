b08646ef10b04811eaea630bb1de3392574951f1
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-02-04 15:38:28 +0900

    불필요한 sudo를 제거합니다

--- delta-web/remote.sh
+++ delta-web/remote.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-sudo docker load --input docker-image.tar && \
-(sudo docker stop delta-web || true) && \
-(sudo docker rm delta-web || true) && \
-sudo docker run -d --restart=unless-stopped \
+docker load --input docker-image.tar && \
+(docker stop delta-web || true) && \
+(docker rm delta-web || true) && \
+docker run -d --restart=unless-stopped \
 --name delta-web delta-web && \
 rm docker-image.tar
