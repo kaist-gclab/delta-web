4f99e57cc4af45c60ee1bcacc058f6be120623e7
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:43:06 +0900

    에셋 유형 목록 페이지 준비

---
+++ delta-web/src/assetType/AssetTypeListPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const AssetTypeListPage: React.FC = () => {
+  return <h1>에셋 유형 목록</h1>;
+};
+
+export default observer(AssetTypeListPage);
