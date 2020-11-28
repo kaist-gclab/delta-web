8bdc7d79527c4beada138c2c678644b83d67af04
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:43:51 +0900

    작업 상세 조회 페이지 준비

---
+++ delta-web/src/job/JobDetailPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const JobDetailPage: React.FC = () => {
+  return <h1>작업 상세 조회</h1>;
+};
+
+export default observer(JobDetailPage);
