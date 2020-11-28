d6074543c541929f05e867f931d9f194422f0eed
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 00:52:41 +0900

    "실패했습니다." 안내 컴포넌트 추가

---
+++ delta-web/src/core/Failed.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Failed: React.FC = () => {
+  return <div className="bp3-non-ideal-state">
+    <div className="bp3-non-ideal-state-visual">
+      <span className="bp3-icon bp3-icon-issue"></span>
+    </div>
+    <h4 className="bp3-heading">실패했습니다.</h4>
+  </div>;
+};
