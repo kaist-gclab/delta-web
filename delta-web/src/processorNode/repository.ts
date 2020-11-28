1b4ebec0b0ca892b0b18dfdb72df00745c06a2b1
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:25:59 +0900

    처리기 노드 목록 Repository 준비

--- delta-web/src/processorNode/repository.ts
+++ delta-web/src/processorNode/repository.ts
@@ -1,8 +1,13 @@
 import Http from '../core/http';
 import { inject, injectable } from 'inversify';
+import { ProcessorNode } from './types';
 
 @injectable()
 export default class ProcessorNodeRepository {
     @inject(Http)
     private http!: Http;
+
+    async fetchAll(): Promise<ProcessorNode[]> {
+        return [];
+    }
 }
