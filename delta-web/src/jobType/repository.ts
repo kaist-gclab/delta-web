b420bf48af2a238ae12095ab4cefd859f588fd16
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:24:22 +0900

    작업 유형 목록 Repository 준비

--- delta-web/src/jobType/repository.ts
+++ delta-web/src/jobType/repository.ts
@@ -1,8 +1,13 @@
 import Http from '../core/http';
 import { inject, injectable } from 'inversify';
+import { JobType } from '../job/types';
 
 @injectable()
 export default class JobTypeRepository {
     @inject(Http)
     private http!: Http;
+
+    async fetchAll(): Promise<JobType[]> {
+        return [];
+    }
 }
