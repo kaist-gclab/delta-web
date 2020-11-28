c5996467210cfce74a58ce7fde7dc2795a1cad10
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:45:15 +0900

    오브젝트 저장소 모니터 페이지 준비

---
+++ delta-web/src/monitoring/ObjectStorageMonitorPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ObjectStorageMonitorPage: React.FC = () => {
+  return <h1>오브젝트 저장소 모니터</h1>;
+};
+
+export default observer(ObjectStorageMonitorPage);
