3a20cef0d7ba97adf54dd42440f62977ead582a3
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:20:18 +0900

    작업 목록 페이지 준비

--- delta-web/src/job/JobListPage.tsx
+++ delta-web/src/job/JobListPage.tsx
@@ -1,8 +1,35 @@
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { JobContext } from '../config/context';
+import { renderCellButton } from '../core/CellButton';
+import { Loading } from '../core/Loading';
+import { Column, Table, Cell, RowHeaderCell } from '@blueprintjs/table';
 
 const JobListPage: React.FC = () => {
-  return <h1>작업 목록</h1>;
+  const store = useContext(JobContext);
+  const history = useHistory();
+  useEffect(() => { /* store.fetchAll(); */ }, [store]);
+
+  const data = store.jobs;
+  if (!data) {
+    return <Loading />;
+  }
+
+  const goDetailPage = (id: number) => {
+    history.push(`/jobs/detail/${id}`);
+  };
+
+  return <div>
+    <h1>작업 목록</h1>
+    <div>
+      <Table numRows={data.length} selectionModes={[]}
+        rowHeaderCellRenderer={(i) => <RowHeaderCell name={data[i].id.toString()} />}>
+        <Column name="TODO" cellRenderer={(i) => <Cell>TODO</Cell>} />
+        <Column name="동작" cellRenderer={(i) => renderCellButton('상세 조회', () => goDetailPage(data[i].id))} />
+      </Table>
+    </div>
+  </div>;
 };
 
 export default observer(JobListPage);
