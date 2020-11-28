f69007758e54e1f34055f3cc8c0e1d0f2249746e
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 23:12:43 +0900

    서버에서 토큰 생성

---
+++ delta-web/src/core/authStore.ts
@@ -0,0 +1,31 @@
+import Axios, { AxiosResponse } from 'axios';
+import { injectable } from 'inversify';
+import { makeAutoObservable } from 'mobx';
+import { AuthBase } from '../config/http';
+import { LoginRequest, LoginResponse } from './types';
+
+@injectable()
+export default class AuthStore {
+    token: string = '';
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    *login(username: string, password: string) {
+        const payload: LoginRequest = { username, password };
+
+        try {
+            const response: AxiosResponse<LoginResponse> =
+                yield Axios.post<LoginResponse>(AuthBase + 'login', payload);
+            this.token = response.data.token;
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    logout() {
+        this.token = '';
+    }
+}
--- delta-web/src/core/tokenService.ts
+++
@@ -1,8 +0,0 @@
-import { injectable } from 'inversify';
-
-@injectable()
-export default class TokenService {
-    getToken() {
-        return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoSW5mbyI6IntcImFjY291bnRcIjp7XCJpZFwiOjEsXCJ1c2VybmFtZVwiOlwiRGVmYXVsdEFkbWluVXNlclwifSxcInJvbGVcIjpcIkFkbWluXCJ9IiwianRpIjoiNjk5YTkwMjU5Y2NhNmM5NGQ2NDRlZjI3NzJlZTI1MTRlNjNhOWJiNzdhZjQ0MmQ5MzVkY2JkZTc2YzI5NDNhYSIsImlzcyI6IkRlbHRhLkFwcFNlcnZlciIsImF1ZCI6IkRlbHRhLkFwcFNlcnZlciJ9.6FjahYC_T1jCE7S8U_FHL-LxoHzptRvDgCtxXM-4UUg';
-    }
-}
