d9cb489164bb59e0af1124b3ae0e2524aec64c46
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:33 +0900

    처리기 노드 상세 조회 페이지 준비

---
+++ delta-web/src/processorNode/ProcessorNodeDetailPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ProcessorNodeDetailPage: React.FC = () => {
+  return <h1>처리기 노드 상세 조회</h1>;
+};
+
+export default observer(ProcessorNodeDetailPage);
