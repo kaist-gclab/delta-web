00ed688361befbf65345e5a05f27631e03b0ce40
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:24:09 +0900

    에셋 유형 목록 Repository 준비

--- delta-web/src/assetType/repository.ts
+++ delta-web/src/assetType/repository.ts
@@ -1,8 +1,13 @@
 import Http from '../core/http';
 import { inject, injectable } from 'inversify';
+import { AssetType } from '../asset/types';
 
 @injectable()
 export default class AssetTypeRepository {
     @inject(Http)
     private http!: Http;
+
+    async fetchAll(): Promise<AssetType[]> {
+        return [];
+    }
 }
