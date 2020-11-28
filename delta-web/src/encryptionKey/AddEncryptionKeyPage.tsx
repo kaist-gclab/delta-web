f77a6ffdfb7363d675ed197ac5ef968e1c185245
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:42 +0900

    암호화 키 추가 페이지 준비

---
+++ delta-web/src/encryptionKey/AddEncryptionKeyPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const AddEncryptionKeyPage: React.FC = () => {
+  return <h1>암호화 키 추가</h1>;
+};
+
+export default observer(AddEncryptionKeyPage);
