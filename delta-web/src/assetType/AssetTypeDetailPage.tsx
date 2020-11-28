552c5714eaa09ea5685caa34e6b0c5ed789adbcc
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:43:13 +0900

    에셋 유형 상세 조회 페이지 준비

---
+++ delta-web/src/assetType/AssetTypeDetailPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const AssetTypeDetailPage: React.FC = () => {
+  return <h1>에셋 유형 상세 조회</h1>;
+};
+
+export default observer(AssetTypeDetailPage);
