40b2ee5073b54605a864edd4ea87a83fd6fb0b1a
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:42:55 +0900

    에셋 유형 추가 페이지 준비

---
+++ delta-web/src/assetType/AddAssetTypePage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const AddAssetTypePage: React.FC = () => {
+  return <h1>에셋 유형 추가</h1>;
+};
+
+export default observer(AddAssetTypePage);
