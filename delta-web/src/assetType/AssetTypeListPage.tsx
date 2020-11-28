f02c197119fe3f217543e27bcb6f0ab03323a35a
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:20:12 +0900

    에셋 유형 목록 페이지 준비

--- delta-web/src/assetType/AssetTypeListPage.tsx
+++ delta-web/src/assetType/AssetTypeListPage.tsx
@@ -1,8 +1,35 @@
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { AssetTypeContext } from '../config/context';
+import { renderCellButton } from '../core/CellButton';
+import { Loading } from '../core/Loading';
+import { Column, Table, Cell, RowHeaderCell } from '@blueprintjs/table';
 
 const AssetTypeListPage: React.FC = () => {
-  return <h1>에셋 유형 목록</h1>;
+  const store = useContext(AssetTypeContext);
+  const history = useHistory();
+  useEffect(() => { /* store.fetchAll(); */ }, [store]);
+
+  const data = store.assetTypes;
+  if (!data) {
+    return <Loading />;
+  }
+
+  const goDetailPage = (id: number) => {
+    history.push(`/asset-types/detail/${id}`);
+  };
+
+  return <div>
+    <h1>에셋 유형 목록</h1>
+    <div>
+      <Table numRows={data.length} selectionModes={[]}
+        rowHeaderCellRenderer={(i) => <RowHeaderCell name={data[i].id.toString()} />}>
+        <Column name="TODO" cellRenderer={(i) => <Cell>TODO</Cell>} />
+        <Column name="동작" cellRenderer={(i) => renderCellButton('상세 조회', () => goDetailPage(data[i].id))} />
+      </Table>
+    </div>
+  </div>;
 };
 
 export default observer(AssetTypeListPage);
