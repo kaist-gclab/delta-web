1a66fd0f4ed20a3fcb85ed6813cf75bb5ce3e3b3
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:10:05 +0900

    단일 작업 Store 준비

--- delta-web/src/job/store.ts
+++ delta-web/src/job/store.ts
@@ -1,12 +1,15 @@
 import { inject, injectable } from 'inversify';
 import { makeAutoObservable } from 'mobx';
 import JobRepository from './repository';
+import { Job } from './types';
 
 @injectable()
 class JobStore {
     constructor() {
         makeAutoObservable(this);
     }
+
+    job?: Job | null;
 
     @inject(JobRepository)
     private jobRepository!: JobRepository
