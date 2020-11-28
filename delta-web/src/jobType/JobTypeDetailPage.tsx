670732f40e4a09f968eafea1edd0af6c7bf25395
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:10 +0900

    작업 유형 상세 조회 페이지 준비

---
+++ delta-web/src/jobType/JobTypeDetailPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const JobTypeDetailPage: React.FC = () => {
+  return <h1>작업 유형 상세 조회</h1>;
+};
+
+export default observer(JobTypeDetailPage);
