54fee27677263394f8ecaebb2c0aca974e987577
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:53:39 +0900

    에셋 유형 추가 요청 보내기

--- delta-web/src/assetType/repository.ts
+++ delta-web/src/assetType/repository.ts
@@ -1,11 +1,16 @@
 import Http from '../core/http';
 import { inject, injectable } from 'inversify';
-import { AssetType } from '../asset/types';
+import { AssetType, CreateAssetTypeRequest } from './types';
 
 @injectable()
 export default class AssetTypeRepository {
     @inject(Http)
     private http!: Http;
+
+    async create(request: CreateAssetTypeRequest): Promise<AssetType> {
+        const response = await this.http.post<AssetType>('asset-types', request);
+        return response.data;
+    }
 
     async fetchAll(): Promise<AssetType[]> {
         const response = await this.http.get<AssetType[]>('asset-types');
