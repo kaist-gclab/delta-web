d9bab830367d801f7b6d27e2a492c15c8bd31442
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:45:09 +0900

    작업 모니터 페이지 준비

---
+++ delta-web/src/monitoring/JobMonitorPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const JobMonitorPage: React.FC = () => {
+  return <h1>작업 모니터</h1>;
+};
+
+export default observer(JobMonitorPage);
