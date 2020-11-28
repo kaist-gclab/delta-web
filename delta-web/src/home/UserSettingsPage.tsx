f61adf2efc0b694c176d6f3962b093c870622212
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:42:09 +0900

    사용자 설정 페이지 준비

---
+++ delta-web/src/home/UserSettingsPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const UserSettingsPages: React.FC = () => {
+  return <h1>사용자 설정</h1>;
+};
+
+export default observer(UserSettingsPages);
