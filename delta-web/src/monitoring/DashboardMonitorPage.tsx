873a2e151e5f3fe3b61a38c0be9378d1bd6dd785
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:45:03 +0900

    모니터링 대시보드 페이지 준비

---
+++ delta-web/src/monitoring/DashboardMonitorPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const DashboardMonitorPage: React.FC = () => {
+  return <h1>모니터링 대시보드</h1>;
+};
+
+export default observer(DashboardMonitorPage);
