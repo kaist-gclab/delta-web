244698ae83ffd3076ae2801b5a6bf64d88084849
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:10:19 +0900

    단일 처리기 노드 Store 준비

--- delta-web/src/processorNode/store.ts
+++ delta-web/src/processorNode/store.ts
@@ -1,12 +1,15 @@
 import { inject, injectable } from 'inversify';
 import { makeAutoObservable } from 'mobx';
 import ProcessorNodeRepository from './repository';
+import { ProcessorNode } from './types';
 
 @injectable()
 class ProcessorNodeStore {
     constructor() {
         makeAutoObservable(this);
     }
+
+    processorNode?: ProcessorNode | null;
 
     @inject(ProcessorNodeRepository)
     private monitoringRepository!: ProcessorNodeRepository
