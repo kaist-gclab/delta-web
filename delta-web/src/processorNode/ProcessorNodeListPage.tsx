ac6466e3e5098361594eff6375e9054ac932b745
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:24 +0900

    처리기 노드 목록 페이지 준비

---
+++ delta-web/src/processorNode/ProcessorNodeListPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ProcessorNodeListPage: React.FC = () => {
+  return <h1>처리기 노드 목록</h1>;
+};
+
+export default observer(ProcessorNodeListPage);
