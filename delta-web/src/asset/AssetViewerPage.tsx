cb5f667635aad1fd4adfd0d3b10122c3d096414f
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:42:45 +0900

    에셋 뷰어 페이지 준비

---
+++ delta-web/src/asset/AssetViewerPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const AssetViewerPage: React.FC = () => {
+  return <h1>에셋 뷰어</h1>;
+};
+
+export default observer(AssetViewerPage);
