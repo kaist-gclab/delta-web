7f7f9b85607f1d6bc5fff9b1c31f614a6cf61f4a
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:00 +0900

    작업 유형 목록 페이지 준비

---
+++ delta-web/src/jobType/JobTypeListPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const JobTypeListPage: React.FC = () => {
+  return <h1>작업 유형 목록</h1>;
+};
+
+export default observer(JobTypeListPage);
