d7c46f5d33e70a82ac9366b3b45b63a6819acbb0
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 23:23:32 +0900

    ProcessorNodeCapability 타입 추가

--- delta-web/src/processorNode/types.ts
+++ delta-web/src/processorNode/types.ts
@@ -1,5 +1,16 @@
+import { AssetType } from '../asset/types';
+import { JobType } from '../job/types';
+
 export interface ProcessorNode {
     id: number;
     key: string;
     name: string | null;
+}
+
+export interface ProcessorNodeCapability {
+    id: number;
+    processorNode: ProcessorNode;
+    jobType: JobType;
+    assetType: AssetType | null;
+    mediaType: string;
 }
