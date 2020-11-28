970180a66c6a5061a718bac2f43b04c3f391bdc7
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:44:55 +0900

    암호화 키 상세 조회 페이지 준비

---
+++ delta-web/src/encryptionKey/EncryptionKeyDetailPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const EncryptionKeyDetailPage: React.FC = () => {
+  return <h1>암호화 키 상세 조회</h1>;
+};
+
+export default observer(EncryptionKeyDetailPage);
