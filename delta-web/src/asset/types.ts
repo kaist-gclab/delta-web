e50c2063d497182f7264d37dd03d12bdc620dc56
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 23:21:25 +0900

    Asset 타입 업데이트

--- delta-web/src/asset/types.ts
+++ delta-web/src/asset/types.ts
@@ -1,9 +1,17 @@
+import { EncryptionKey } from '../encryptionKey/types';
+import { JobExecution } from '../job/types';
+
 export type Assets = Asset[];
 
 export interface Asset {
     id: number;
+    assetType: AssetType;
+    encryptionKey: EncryptionKey | null;
+    mediaType: string;
     storeKey: string;
     assetTags: AssetTag[];
+    parentJobExecution: JobExecution | null;
+    createdAt: string;
 }
 
 export interface AssetTag {
