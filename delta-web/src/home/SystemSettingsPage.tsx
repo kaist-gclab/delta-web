0176c1413dfd7108b8d06ea12d7d96d342e2f26c
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:42:15 +0900

    시스템 설정 페이지 준비

---
+++ delta-web/src/home/SystemSettingsPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const SystemSettingsPage: React.FC = () => {
+  return <h1>시스템 설정</h1>;
+};
+
+export default observer(SystemSettingsPage);
