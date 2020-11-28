ea8e7d1ae38496dd72e0d9fb3425d00283ef5aa6
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 00:53:45 +0900

    상세 조회 화면으로 바로 가기

--- delta-web/src/encryptionKey/EncryptionKeyListPage.tsx
+++ delta-web/src/encryptionKey/EncryptionKeyListPage.tsx
@@ -3,15 +3,22 @@ import { observer } from 'mobx-react';
 import { Loading } from '../core/Loading';
 import { EncryptionKeyContext } from '../config/context';
 import { Column, Table, Cell, RowHeaderCell } from '@blueprintjs/table';
+import { renderCellButton } from '../core/CellButton';
+import { useHistory } from 'react-router';
 
 const EncryptionKeyListPage: React.FC = () => {
   const enc = useContext(EncryptionKeyContext);
-  useEffect(() => { enc.fetchEncryptionKeys(); }, [enc]);
+  const history = useHistory();
+  useEffect(() => { enc.fetchAll(); }, [enc]);
 
   const data = enc.encryptionKeys;
   if (!data) {
     return <Loading />;
   }
+
+  const goDetailPage = (id: number) => {
+    history.push(`/encryption-keys/detail/${id}`);
+  };
 
   return <div>
     <h1>암호화 키 목록</h1>
@@ -20,6 +27,7 @@ const EncryptionKeyListPage: React.FC = () => {
         rowHeaderCellRenderer={(i) => <RowHeaderCell name={data[i].id.toString()} />}>
         <Column name="이름" cellRenderer={(i) => <Cell>{data[i].name}</Cell>} />
         <Column name="활성 상태" cellRenderer={(i) => <Cell>{data[i].enabled ? '예' : '아니오'}</Cell>} />
+        <Column name="동작" cellRenderer={(i) => renderCellButton('상세 조회', () => goDetailPage(data[i].id))} />
       </Table>
     </div>
   </div>;
