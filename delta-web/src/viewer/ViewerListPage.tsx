b2512764b2d93cd3dc6cf5bb052a403eaca3de73
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:43:23 +0900

    에셋 뷰어 목록 페이지 준비

---
+++ delta-web/src/viewer/ViewerListPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ViewerListPage: React.FC = () => {
+  return <h1>뷰어 목록</h1>;
+};
+
+export default observer(ViewerListPage);
