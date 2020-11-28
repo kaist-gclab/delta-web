2906126b4b7d2724bca216dc176fa30a1c9b076e
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-28 16:45:21 +0900

    처리기 노드 모니터 페이지 준비

---
+++ delta-web/src/monitoring/ProcessorNodeMonitorPage.tsx
@@ -0,0 +1,8 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+const ProcessorNodeMonitorPage: React.FC = () => {
+  return <h1>처리기 노드 모니터</h1>;
+};
+
+export default observer(ProcessorNodeMonitorPage);
