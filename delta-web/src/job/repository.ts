4054beb03928ce1e6a94242529aec6246034f6ae
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-11-29 02:53:46 +0900

    작업 추가 요청 보내기

--- delta-web/src/job/repository.ts
+++ delta-web/src/job/repository.ts
@@ -1,11 +1,16 @@
 import Http from '../core/http';
 import { inject, injectable } from 'inversify';
-import { Job } from './types';
+import { CreateJobRequest, Job } from './types';
 
 @injectable()
 export default class JobRepository {
     @inject(Http)
     private http!: Http;
+
+    async create(request: CreateJobRequest): Promise<Job> {
+        const response = await this.http.post<Job>('jobs', request);
+        return response.data;
+    }
 
     async fetchAll(): Promise<Job[]> {
         const response = await this.http.get<Job[]>('jobs');
