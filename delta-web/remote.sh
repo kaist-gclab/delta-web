46971644bb97b753d2097211886033ff698ab5f8
Hyeonguk Ryu <ryu@hyeonguk.com>
2020-12-09 13:06:24 +0900

    로그 크기 제한, 외부 환경 변수 입력 기능 추가

--- delta-web/remote.sh
+++ delta-web/remote.sh
@@ -4,5 +4,7 @@ IMAGE="delta-web"
 (docker stop $IMAGE || true) && \
 (docker rm $IMAGE || true) && \
 docker run --init -d \
+$DELTA_WEB_RUN_OPTIONS \
 --restart=unless-stopped \
+--log-opt max-size=16m --log-opt max-file=8 \
 --name $IMAGE $IMAGE
