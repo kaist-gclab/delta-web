d2e5b6f907265fdf83d74956c7a68c5b97d1e607
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-07 16:13:13 +0900

    Day.js로 전환

--- delta-web/src/core/datetime.ts
+++ delta-web/src/core/datetime.ts
@@ -1,18 +1,17 @@
-import moment from 'moment';
+import dayjs from 'dayjs';
 
 export function localDate(value: string): string {
-    return moment(value).format('YYYY-MM-DD');
+    return dayjs(value).format('YYYY-MM-DD');
 }
 
 export function localDateTime(value: string): string {
-    return moment(value).format('YYYY-MM-DD HH:mm:ss');
+    return dayjs(value).format('YYYY-MM-DD HH:mm:ss');
 }
 
 export function localDateTimeShort(value: string): string {
-    return moment(value).format('MM-DD HH:mm:ss');
+    return dayjs(value).format('MM-DD HH:mm:ss');
 }
 
 export function localDateTimeMicro(value: string): string {
-
-    return moment(value).format('YYYY-MM-DD HH:mm:ss.SSS');
+    return dayjs(value).format('YYYY-MM-DD HH:mm:ss.SSS');
 }
