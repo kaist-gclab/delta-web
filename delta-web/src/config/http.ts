6c91df93f778225c543d49b3dfef32a19ac7d4ee
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-07 16:19:48 +0900

    ImportMetaEnv 적용

--- delta-web/src/config/http.ts
+++ delta-web/src/config/http.ts
@@ -1,3 +1,3 @@
-export const ApiBase: string = process.env.REACT_APP_API_BASE!;
-export const AuthBase: string = process.env.REACT_APP_AUTH_BASE!;
-export const InitialToken: string = process.env.REACT_APP_INITIAL_TOKEN!;
+export const ApiBase: string = import.meta.env.VITE_API_BASE;
+export const AuthBase: string = import.meta.env.VITE_AUTH_BASE;
+export const InitialToken: string = import.meta.env.VITE_INITIAL_TOKEN;
--- delta-web/src/vite-env.d.ts
+++ delta-web/src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+    VITE_API_BASE: string;
+    VITE_AUTH_BASE: string;
+    VITE_INITIAL_TOKEN: string;
+}
