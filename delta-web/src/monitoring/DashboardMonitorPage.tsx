37de653f5481032f22eb7a46967628241253f54b
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:21:45 +0900

    필요한 import 추가

--- delta-web/src/monitoring/DashboardMonitorPage.tsx
+++ delta-web/src/monitoring/DashboardMonitorPage.tsx
@@ -1,8 +1,11 @@
 import { Table2, RowHeaderCell, Column, Cell } from '@blueprintjs/table';
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import styled from '@emotion/styled';
+import { JobContext } from '../config/context';
+import { Row as GridRow, Column as GridColumn } from '../core/Grid';
+import { Loading } from '../core/Loading';
 
 const DashboardMonitorPage: React.FC = () => {
   return <h1>모니터링 대시보드</h1>;
