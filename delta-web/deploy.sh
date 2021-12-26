9ea9ab3c74053650ab4efe914947e230c85a998d
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:14:44 +0900

    배포 스크립트 Format

--- delta-web/deploy.sh
+++ delta-web/deploy.sh
@@ -2,5 +2,5 @@
 IMAGE="delta-web"
 REMOTE="delta-test"
 
-docker save $IMAGE | lz4 | ssh $REMOTE "lz4 -dc | docker load" && \
-cat remote.sh | ssh $REMOTE
+docker save $IMAGE | lz4 | ssh $REMOTE "lz4 -dc | docker load" &&
+    cat remote.sh | ssh $REMOTE
