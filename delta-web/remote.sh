58469b2ee51a52fb005d5d62f9d5426fb3c4b426
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:14:51 +0900

    원격 스크립트 Format

--- delta-web/remote.sh
+++ delta-web/remote.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 IMAGE="delta-web"
 
-(docker stop $IMAGE || true) && \
-(docker rm $IMAGE || true) && \
-docker run --init -d \
-$DELTA_WEB_RUN_OPTIONS \
---restart=unless-stopped \
---log-opt max-size=16m --log-opt max-file=8 \
---name $IMAGE $IMAGE
+(docker stop $IMAGE || true) &&
+    (docker rm $IMAGE || true) &&
+    docker run --init -d \
+        $DELTA_WEB_RUN_OPTIONS \
+        --restart=unless-stopped \
+        --log-opt max-size=16m --log-opt max-file=8 \
+        --name $IMAGE $IMAGE
