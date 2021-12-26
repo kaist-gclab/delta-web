75b7a773b3e1fba54e8e01702b0abf30c55739aa
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:23:43 +0900

    뷰어 목록 페이지에서 Table을 Table2로 변경

--- delta-web/src/viewer/ViewerListPage.tsx
+++ delta-web/src/viewer/ViewerListPage.tsx
@@ -1,4 +1,4 @@
-import { Table, RowHeaderCell, Column, Cell } from '@blueprintjs/table';
+import { Table2, RowHeaderCell, Column, Cell } from '@blueprintjs/table';
 import { observer } from 'mobx-react';
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
