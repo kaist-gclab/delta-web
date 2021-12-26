9af54bd36353316828051e12d8272ae6226b96da
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 13:03:11 +0900

    JobType Id 타입 변경 반영

--- delta-web/src/jobType/JobTypeDetailPage.tsx
+++ delta-web/src/jobType/JobTypeDetailPage.tsx
@@ -24,7 +24,7 @@ const JobTypeDetailPage: React.FC = () => {
   const store = useContext(JobTypeContext);
   useEffect(() => {
     if (params.id) {
-      store.fetch(Number(params.id));
+      store.fetch(params.id);
     }
   }, [store, params]);
 
