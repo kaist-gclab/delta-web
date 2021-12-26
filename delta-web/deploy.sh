29b7879a2279d893034828a7ba01cf2475cf84eb
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 13:14:16 +0900

    자동 배포 구성 대비

--- delta-web/deploy.sh
+++ delta-web/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 IMAGE="delta-web"
-REMOTE="delta-test"
+REMOTE="${REMOTE:-delta-test}"
 
 docker save $IMAGE | lz4 | ssh $REMOTE "lz4 -dc | docker load" &&
     cat remote.sh | ssh $REMOTE
