d13c7ebe5eecb656681b9b7b0e24c21258ef8056
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:29:40 +0900

    작업 상태 가져오기

--- delta-web/src/monitoring/ObjectStorageMonitorPage.tsx
+++ delta-web/src/monitoring/ObjectStorageMonitorPage.tsx
@@ -6,8 +6,29 @@ import { JobContext } from '../config/context';
 import { Loading } from '../core/Loading';
 
 const ObjectStorageMonitorPage: React.FC = () => {
-  return <h1>오브젝트 저장소 모니터</h1>;
+  const store = useContext(JobContext);
   const navigate = useNavigate();
+  useEffect(() => { store.fetchAll(); }, [store]);
+
+  const data = store.jobs;
+  if (!data) {
+    return <Loading />;
+  }
+
+  const goDetailPage = (id: number) => {
+    navigate(`/jobs/detail/${id}`);
+  };
+
+  return <div>
+    <h1>오브젝트 저장소 모니터</h1>
+    <div>
+      <Table2 numRows={data.length} selectionModes={[]}
+        rowHeaderCellRenderer={(i) => <RowHeaderCell name={data[i].id.toString()} />}>
+        <Column name="타임스탬프" cellRenderer={(i) => <Cell>{data[i].jobType.key}</Cell>} />
+        <Column name="내용" cellRenderer={(i) => <Cell>{data[i].jobType.name}</Cell>} />
+      </Table2>
+    </div>
+  </div>;
 };
 
 export default observer(ObjectStorageMonitorPage);
