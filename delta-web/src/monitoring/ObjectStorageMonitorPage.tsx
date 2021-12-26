8233b4e0895d84ae063e2f08eec144458d5c03b6
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 12:29:24 +0900

    JobContext 추가

--- delta-web/src/monitoring/ObjectStorageMonitorPage.tsx
+++ delta-web/src/monitoring/ObjectStorageMonitorPage.tsx
@@ -1,7 +1,9 @@
 import { Table2, RowHeaderCell, Column, Cell } from '@blueprintjs/table';
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
+import { JobContext } from '../config/context';
+import { Loading } from '../core/Loading';
 
 const ObjectStorageMonitorPage: React.FC = () => {
   return <h1>오브젝트 저장소 모니터</h1>;
