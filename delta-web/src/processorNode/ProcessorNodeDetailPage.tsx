b5d0d53752daffd6ee0f93d4f05573a7038e7864
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 13:03:40 +0900

    ProcessorNode Id 타입 변경 반영

--- delta-web/src/processorNode/ProcessorNodeDetailPage.tsx
+++ delta-web/src/processorNode/ProcessorNodeDetailPage.tsx
@@ -24,7 +24,7 @@ const ProcessorNodeDetailPage: React.FC = () => {
   const store = useContext(ProcessorNodeContext);
   useEffect(() => {
     if (params.id) {
-      store.fetch(Number(params.id));
+      store.fetch(params.id);
     }
   }, [store, params]);
 
