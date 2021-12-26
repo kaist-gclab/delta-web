9e53b58ec611110f02a620cdfad4cc2d40cc0b1a
Hyeonguk Ryu <ryu@hyeonguk.com>
2021-12-26 13:27:27 +0900

    작업 추가 페이지 전환 준비

--- delta-web/src/job/AddJobPage.tsx
+++ delta-web/src/job/AddJobPage.tsx
@@ -1,5 +1,6 @@
+import { FormGroup, InputGroup, Button, Dialog, Classes } from '@blueprintjs/core';
 import { observer } from 'mobx-react';
-import React from 'react';
+import React, { FormEvent, useContext, useState } from 'react';
 
 const AddJobPage: React.FC = () => {
   return <h1>작업 추가</h1>;
