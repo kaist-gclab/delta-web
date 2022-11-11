6545faac84d190172d17f8f9cf32a3133465f3bf
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-11 22:56:04 +0900

    App에 SWR fetcher 적용

--- delta-web/src/App.tsx
+++ delta-web/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 import { configure } from './config/context';
+import fetcher from './core/fetcher';
 import Layout from './core/Layout';
 
 const App: React.FC = () => {
-  return <Router>
-    <Layout />
-  </Router>;
+  return <SWRConfig value={{ fetcher }}>
+    <Router>
+      <Layout />
+    </Router>
+  </SWRConfig>;
 };
 
 export default configure(App);
