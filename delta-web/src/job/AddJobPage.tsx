611f2cafb405a09d9bb4a832f9ba1f8fa5a11638
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-11 23:11:08 +0900

    불필요한 코드 제거

--- delta-web/src/job/AddJobPage.tsx
+++ delta-web/src/job/AddJobPage.tsx
@@ -1,6 +1,6 @@
 import { FormGroup, InputGroup, Button, Dialog, Classes } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
-import React, { FormEvent, useContext, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 
 const AddJobPage: React.FC = () => {
   return <h1>작업 추가</h1>;
