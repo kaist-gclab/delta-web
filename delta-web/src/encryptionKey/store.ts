3b8ec87d455cf8a680bfc71c08f3104c733f2fc7
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-14 15:29:25 +0900

    EncryptionKeyRepository 제거

--- delta-web/src/encryptionKey/store.ts
+++ delta-web/src/encryptionKey/store.ts
@@ -1,31 +1,28 @@
 import { makeAutoObservable, runInAction } from 'mobx';
-import EncryptionKeyRepository from './repository';
-import { CreateEncryptionKeyRequest, CreateEncryptionKeyResponse, EncryptionKey } from './types';
+import { CreateEncryptionKeyRequest, CreateEncryptionKeyResponse, EncryptionKey, EncryptionKeys } from '../api';
 
 class EncryptionKeyStore {
     encryptionKeys?: EncryptionKey[];
     encryptionKey?: EncryptionKey | null;
-
-    private encryptionKeyRepository = EncryptionKeyRepository;
 
     constructor() {
         makeAutoObservable(this);
     }
 
     async create(request: CreateEncryptionKeyRequest) {
-        const result: CreateEncryptionKeyResponse = await this.encryptionKeyRepository.create(request);
+        const result: CreateEncryptionKeyResponse = await EncryptionKeys.create(request);
         return result;
     }
 
     async fetchAll() {
-        const encryptionKeys = await this.encryptionKeyRepository.fetchAll();
+        const encryptionKeys = await EncryptionKeys.getEncryptionKeys();
         runInAction(() => { this.encryptionKeys = encryptionKeys?.sort((a, b) => Number(a.id) - Number(b.id)); });
     }
 
     async fetch(id: string) {
         this.encryptionKey = undefined;
         await this.fetchAll();
-        runInAction(() => { this.encryptionKey = this.encryptionKeys?.find(e => e.id === id) ?? null; });
+        runInAction(() => { this.encryptionKey = this.encryptionKeys?.find(e => e.id.toString() === id) ?? null; });
     }
 }
 
