0ae44484544dd718f3bebaf6970a339dd6c49509
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-14 15:24:53 +0900

    HomeRepository 제거

--- delta-web/src/home/repository.ts
+++
@@ -1,7 +0,0 @@
-import Http from '../core/http';
-
-class HomeRepository {
-    private http = Http;
-}
-
-export default new HomeRepository();
--- delta-web/src/home/store.ts
+++ delta-web/src/home/store.ts
@@ -1,12 +1,9 @@
 import { makeAutoObservable } from 'mobx';
-import HomeRepository from './repository';
 
 class HomeStore {
     constructor() {
         makeAutoObservable(this);
     }
-
-    private homeRepository = HomeRepository;
 }
 
 export default new HomeStore();
