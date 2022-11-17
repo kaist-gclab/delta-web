a7445b2c11a6e2f0a735012a6bf0d867055d458c
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 19:50:30 +0900

    job에서 사용하지 않는 import 삭제

--- delta-web/src/job/AddJobPage.tsx
+++ delta-web/src/job/AddJobPage.tsx
@@ -1,6 +1,6 @@
 import { FormGroup, InputGroup, Button, Dialog, Classes } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
-import React, { FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 
   return <h1>작업 추가</h1>;
 function AddJobPage() {
