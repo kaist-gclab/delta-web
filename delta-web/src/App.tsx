f4b7a5249f70616aa174a3b6af94457ca91e7933
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 18:13:34 +0900

    App에서 React.FC 제거

--- delta-web/src/App.tsx
+++ delta-web/src/App.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 import { configure } from './config/context';
 import fetcher from './core/fetcher';
 import Layout from './core/Layout';
 
-const App: React.FC = () => {
+function App() {
   return <SWRConfig value={{ fetcher }}>
     <Router>
       <Layout />
     </Router>
   </SWRConfig>;
-};
+}
 
 export default configure(App);
