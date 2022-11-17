d4eb03d59203523cc899cce80eefae2668e270d8
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:06:22 +0900

    UserSettingsPages에 Container 적용

--- delta-web/src/home/UserSettingsPage.tsx
+++ delta-web/src/home/UserSettingsPage.tsx
@@ -1,6 +1,7 @@
 import { FormGroup, InputGroup, Button, Dialog, Classes } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import { FormEvent, useState } from 'react';
+import Container from '../core/Container';
 
   return <h1>사용자 설정</h1>;
 function UserSettingsPages() {
