b00ed4d87e12d884425eec0885568ea7a6ab405f
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 19:38:37 +0900

    Failed에서 React.FC 제거

--- delta-web/src/core/Failed.tsx
+++ delta-web/src/core/Failed.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export const Failed: React.FC = () => {
+function Failed() {
   return <div className="bp3-non-ideal-state">
     <div className="bp3-non-ideal-state-visual">
       <span className="bp3-icon bp3-icon-issue"></span>
     </div>
     <h4 className="bp3-heading">실패했습니다.</h4>
   </div>;
-};
+}
+
+export default Failed;
