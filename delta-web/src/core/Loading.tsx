42b75eb1b77c12750f063a183ca5bb2fd2350e4c
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 19:38:45 +0900

    Loading에서 React.FC 제거

--- delta-web/src/core/Loading.tsx
+++ delta-web/src/core/Loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const Loading: React.FC = () => {
+function Loading() {
   return <div className="bp3-non-ideal-state">
     <div className="bp3-non-ideal-state-visual">
       <span className="bp3-icon bp3-icon-refresh"></span>
@@ -8,4 +8,6 @@ export const Loading: React.FC = () => {
     <h4 className="bp3-heading">불러오는 중</h4>
     <div>잠시만 기다려 주세요.</div>
   </div>;
-};
+}
+
+export default Loading;
