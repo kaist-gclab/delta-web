fab58a9223b3b87da1aeb886c62d65156753697c
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 19:50:56 +0900

    processorNode에서 사용하지 않는 import 삭제

--- delta-web/src/processorNode/ProcessorNodeListPage.tsx
+++ delta-web/src/processorNode/ProcessorNodeListPage.tsx
@@ -1,5 +1,5 @@
 import { observer } from 'mobx-react-lite';
-import React, { useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { ProcessorNodeContext } from '../config/context';
 import { renderCellButton } from '../core/CellButton';
