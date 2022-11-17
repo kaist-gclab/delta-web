6a384660de8a365e22d1239b631cc0d521a53812
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 20:49:01 +0900

    AssetDetailPage Lazy Loading 적용

--- delta-web/src/core/Layout.tsx
+++ delta-web/src/core/Layout.tsx
@@ -1,7 +1,6 @@
 import { lazy, useContext } from 'react';
 import { Route, Routes } from 'react-router';
 import styled from '@emotion/styled';
-import AssetDetailPage from '../asset/AssetDetailPage';
 import AssetViewerPage from '../asset/AssetViewerPage';
 import AssetTypeListPage from '../assetType/AssetTypeListPage';
 import AddAssetTypePage from '../assetType/AddAssetTypePage';
@@ -31,6 +30,7 @@ const StartPage = lazy(() => import('../home/StartPage'));
 const HelpPage = lazy(() => import('../home/HelpPage'));
 const UserSettingsPage = lazy(() => import('../home/UserSettingsPage'));
 const SystemSettingsPage = lazy(() => import('../home/SystemSettingsPage'));
+const AssetDetailPage = lazy(() => import('../asset/AssetDetailPage'));
 const LoginPage = lazy(() => import('../home/LoginPage'));
 
 
