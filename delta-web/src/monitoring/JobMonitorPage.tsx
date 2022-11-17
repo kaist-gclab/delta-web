4a7ab7befbf626d270df948fdadcde468d6c984f
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 18:10:52 +0900

    작업 모니터를 HTMLTable로 전환

--- delta-web/src/monitoring/JobMonitorPage.tsx
+++ delta-web/src/monitoring/JobMonitorPage.tsx
@@ -1,11 +1,45 @@
+import { HTMLTable } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router';
+import { JobContext } from '../config/context';
 import { Loading } from '../core/Loading';
 
 const JobMonitorPage: React.FC = () => {
-  return <h1>작업 모니터</h1>;
+  const store = useContext(JobContext);
   const navigate = useNavigate();
+  useEffect(() => { store.fetchAll(); }, [store]);
+
+  const data = store.jobs;
+  if (!data) {
+    return <Loading />;
+  }
+
+  const goDetailPage = (id: string) => {
+    navigate(`/jobs/detail/${id}`);
+  };
+
+  return <div>
+    <h1>작업 모니터</h1>
+    <div>
+      <HTMLTable>
+        <thead>
+          <tr>
+            <th>타임스탬프</th>
+            <th>인자</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((job) => {
+            return <tr key={job.id.toString()}>
+              <td>{job.createdAt.toISOString()}</td>
+              <td>{job.jobArguments}</td>
+            </tr>;
+          })};
+        </tbody>
+      </HTMLTable>
+    </div>
+  </div>;
 };
 
 export default observer(JobMonitorPage);
