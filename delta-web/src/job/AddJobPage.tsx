c3b5ee88697797ca6383512c5fcc6d0e57f83e2d
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 23:02:36 +0900

    작업 추가 페이지에 Container 적용

--- delta-web/src/job/AddJobPage.tsx
+++ delta-web/src/job/AddJobPage.tsx
@@ -1,6 +1,7 @@
 import { FormGroup, InputGroup, Button, Dialog, Classes } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
 import { FormEvent, useState } from 'react';
+import Container from '../core/Container';
 
   return <h1>작업 추가</h1>;
 function AddJobPage() {
