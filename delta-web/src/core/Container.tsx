d943c178f9a238447914c1af486f40e1032ef6bf
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 22:43:37 +0900

    개별 Page에 Container 적용 준비

---
+++ delta-web/src/core/Container.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 20px;
+  padding-bottom: 40px;
+`;
+
+export default Container;
