0e66a47bd89d33d9a87adbdfd6d0cd6029b80f0b
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-17 19:39:36 +0900

    JobTypeDetailPage에서 React.FC 제거

--- delta-web/src/jobType/JobTypeDetailPage.tsx
+++ delta-web/src/jobType/JobTypeDetailPage.tsx
@@ -16,7 +16,7 @@ const Message = styled.div`
 margin: 20px 0;
 `;
 
-const JobTypeDetailPage: React.FC = () => {
+function JobTypeDetailPage() {
   const params = useParams<'id'>();
   const navigate = useNavigate();
   const [id, setId] = useState('');
@@ -57,6 +57,6 @@ const JobTypeDetailPage: React.FC = () => {
     </ControlGroup>
     {!params.id ? <Message>번호로 조회하십시오.</Message> : render()}
   </div>;
-};
+}
 
 export default observer(JobTypeDetailPage);
