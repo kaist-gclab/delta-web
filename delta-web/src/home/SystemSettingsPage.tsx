ab4ffa2f86c8c708b53a49a82ce5ddb1bed8e814
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 15:21:03 +0900

    신규 API 적용 준비

--- delta-web/src/home/SystemSettingsPage.tsx
+++ delta-web/src/home/SystemSettingsPage.tsx
@@ -3,8 +3,65 @@ import { observer } from 'mobx-react-lite';
 import { FormEvent, useState } from 'react';
 import Container from '../core/Container';
 
-  return <h1>시스템 설정</h1>;
 function SystemSettingsPage() {
+  const [key, setKey] = useState('');
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false);
+  const [okDialogOpen, setOkDialogOpen] = useState(false);
+  const openOkDialog = () => setOkDialogOpen(true);
+  const openErrorDialog = () => setErrorDialogOpen(true);
+  const closeOkDialog = () => setOkDialogOpen(false);
+  const closeErrorDialog = () => setErrorDialogOpen(false);
+
+  const submit = async () => {
+    try {
+      openOkDialog();
+    } catch {
+      openErrorDialog();
+    }
+  };
+
+  return <Container>
+    <h1>시스템 설정</h1>
+    <FormGroup
+      label="처리기 최대 응답 시간"
+      labelInfo="(필수)"
+      helperText="처리기 최대 응답 시간"
+      labelFor="input-key">
+      <InputGroup id="input-key" placeholder="초" value={key}
+        onChange={(e: FormEvent<HTMLInputElement>) => { setKey(e.currentTarget.value); }} />
+    </FormGroup>
+    <Button large={true} intent="primary" onClick={submit}>저장</Button>
+    <Dialog
+      icon="tick"
+      isOpen={okDialogOpen}
+      onClose={closeOkDialog}
+      title="추가 성공"
+      canOutsideClickClose={false}>
+      <div className={Classes.DIALOG_BODY}>
+        <p>성공적으로 추가되었습니다.</p>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={closeOkDialog}>닫기</Button>
+        </div>
+      </div>
+    </Dialog>
+    <Dialog
+      icon="issue"
+      isOpen={errorDialogOpen}
+      onClose={closeErrorDialog}
+      title="추가 실패"
+      canOutsideClickClose={false}>
+      <div className={Classes.DIALOG_BODY}>
+        <p>추가하는 데 실패했습니다.</p>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={closeErrorDialog}>닫기</Button>
+        </div>
+      </div>
+    </Dialog>
+  </Container>;
 }
 
 export default observer(SystemSettingsPage);
--- delta-web/src/home/UserSettingsPage.tsx
+++ delta-web/src/home/UserSettingsPage.tsx
@@ -3,8 +3,65 @@ import { observer } from 'mobx-react-lite';
 import { FormEvent, useState } from 'react';
 import Container from '../core/Container';
 
-  return <h1>사용자 설정</h1>;
 function UserSettingsPage() {
+  const [key, setKey] = useState('');
+  const [errorDialogOpen, setErrorDialogOpen] = useState(false);
+  const [okDialogOpen, setOkDialogOpen] = useState(false);
+  const openOkDialog = () => setOkDialogOpen(true);
+  const openErrorDialog = () => setErrorDialogOpen(true);
+  const closeOkDialog = () => setOkDialogOpen(false);
+  const closeErrorDialog = () => setErrorDialogOpen(false);
+
+  const submit = async () => {
+    try {
+      openOkDialog();
+    } catch {
+      openErrorDialog();
+    }
+  };
+
+  return <Container>
+    <h1>사용자 설정</h1>
+    <FormGroup
+      label="새 암호"
+      labelInfo="(필수)"
+      helperText="새 암호"
+      labelFor="input-key">
+      <InputGroup id="input-key" placeholder="암호" value={key}
+        onChange={(e: FormEvent<HTMLInputElement>) => { setKey(e.currentTarget.value); }} />
+    </FormGroup>
+    <Button large={true} intent="primary" onClick={submit}>암호 변경</Button>
+    <Dialog
+      icon="tick"
+      isOpen={okDialogOpen}
+      onClose={closeOkDialog}
+      title="추가 성공"
+      canOutsideClickClose={false}>
+      <div className={Classes.DIALOG_BODY}>
+        <p>성공적으로 추가되었습니다.</p>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={closeOkDialog}>닫기</Button>
+        </div>
+      </div>
+    </Dialog>
+    <Dialog
+      icon="issue"
+      isOpen={errorDialogOpen}
+      onClose={closeErrorDialog}
+      title="추가 실패"
+      canOutsideClickClose={false}>
+      <div className={Classes.DIALOG_BODY}>
+        <p>추가하는 데 실패했습니다.</p>
+      </div>
+      <div className={Classes.DIALOG_FOOTER}>
+        <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+          <Button onClick={closeErrorDialog}>닫기</Button>
+        </div>
+      </div>
+    </Dialog>
+  </Container>;
 }
 
 export default observer(UserSettingsPage);
