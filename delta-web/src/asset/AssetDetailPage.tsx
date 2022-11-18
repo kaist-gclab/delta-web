f9a09b80db9445a1b28304eb0301e77a8334d2bb
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 16:23:10 +0900

    애셋 상세 조회 페이지 h1을 PageHeader로 전환

--- delta-web/src/asset/AssetDetailPage.tsx
+++ delta-web/src/asset/AssetDetailPage.tsx
@@ -7,6 +7,7 @@ import { AssetContext } from '../config/context';
 import { Failed } from '../core/Failed';
 import { Loading } from '../core/Loading';
 import Container from '../core/Container';
+import PageHeader from '../core/PageHeader';
 
 const QueryButton = styled(Button)`
 padding-left: 20px;
@@ -45,7 +46,7 @@ function AssetDetailPage() {
   const goDetailPage = () => navigate(`/assets/detail/${id}`);
 
   return <Container>
-    <h1>애셋 상세 조회</h1>
+    <PageHeader>애셋 상세 조회</PageHeader>
     <ControlGroup>
       <InputGroup type="text" leftIcon="numbered-list"
         placeholder="번호" value={id}
