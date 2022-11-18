8cdd4c43d2acba4a84fb32dd5568675cc77fe112
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 15:18:34 +0900

    신규 API 적용 준비, 테스트용 데이터 사용

--- delta-web/src/asset/AssetListPage.tsx
+++ delta-web/src/asset/AssetListPage.tsx
@@ -28,28 +28,11 @@ function getTag(a: Asset, key: string) {
 
 let timeBegin = 0;
 
-  return <h1>에셋 목록</h1>;
-  const assetStore = useContext(AssetContext);
-  const [elapsed, setElapsed] = useState<number | null>(null);
-  const [dialogOpen, setDialogOpen] = useState<number | null>(null);
-
-  function renderAsset(a: Asset) {
-    const url = getTag(a, 'Image');
-    return <p key={a.id}>
-      모델 번호: {a.id},
-      모델 이름: {getTag(a, 'Name')},
-      모델에 지정된 태그: {getTag(a, 'Tag')}
-      {url !== '없음' ? <>
-        <Button style={{ marginLeft: '10px' }} small={true} onClick={() => setDialogOpen(a.id)}>렌더링 결과 이미지 보기</Button>
-        <Dialog style={{ padding: 0 }} isOpen={dialogOpen === a.id} onClose={() => setDialogOpen(null)}>
-          <img src={url} style={{ margin: '0' }} />
-          <Button style={{ marginLeft: 'auto', marginRight: 'auto', width: '50%', marginTop: '10px', marginBottom: '10px' }}
-            onClick={() => setDialogOpen(null)}>닫기</Button>
-        </Dialog>
-      </> : null}
-    </p>;
 function AssetListPage() {
+  const store = useContext(JobContext);
   const navigate = useNavigate();
+  useEffect(() => { store.fetchAll(); }, [store]);
+
   const data: Asset[] = [
     {
       id: BigInt(1),
@@ -90,10 +73,6 @@ function AssetListPage() {
     return <Loading />;
   }
 
-  const goDetailPage = (id: string) => {
-    navigate(`/jobs/detail/${id}`);
-  };
-
   return <Container>
     <h1>애셋 목록</h1>
     <div>
@@ -117,65 +96,12 @@ function AssetListPage() {
             <td>{a.mediaType}</td>
             <td>{a.encryptionKey?.name ?? '없음'}</td>
             <td>{a.storeKey}</td>
-            <td>{renderCellButton('상세 조회', () => goDetailPage(data[i].id.toString()))}</td>
+            <td>{renderCellButton('상세 조회', () => { })}</td>
           </tr>)}
         </tbody>
       </HTMLTable>
     </div>
   </Container>;
-
-  // const assetStore = useContext(AssetContext);
-  // const [elapsed, setElapsed] = useState<number | null>(null);
-  // const [dialogOpen, setDialogOpen] = useState<number | null>(null);
-
-  // function renderAsset(a: Asset) {
-  //   const url = getTag(a, 'Image');
-  //   return <p key={a.id}>
-  //     모델 번호: {a.id},
-  //     모델 이름: {getTag(a, 'Name')},
-  //     모델에 지정된 태그: {getTag(a, 'Tag')}
-  //     {url !== '없음' ? <>
-  //       <Button style={{ marginLeft: '10px' }} small={true} onClick={() => setDialogOpen(a.id)}>렌더링 결과 이미지 보기</Button>
-  //       <Dialog style={{ padding: 0 }} isOpen={dialogOpen === a.id} onClose={() => setDialogOpen(null)}>
-  //         <img src={url} style={{ margin: '0' }} />
-  //         <Button style={{ marginLeft: 'auto', marginRight: 'auto', width: '50%', marginTop: '10px', marginBottom: '10px' }}
-  //           onClick={() => setDialogOpen(null)}>닫기</Button>
-  //       </Dialog>
-  //     </> : null}
-  //   </p>;
-  // }
-
-  // const fetch = async (name: string | null, tag: string | null) => {
-  //   setElapsed(null);
-  //   timeBegin = +new Date();
-  //   await assetStore.fetchAll(name, tag);
-  //   setElapsed((+new Date()) - timeBegin);
-  // };
-
-  // useEffect(() => {
-  //   fetch(null, null);
-  // }, [assetStore]);
-
-  // const [searchKeyword, setSearchKeyword] = useState('');
-  // const assets = assetStore.assets;
-
-
-  // if (!assets) {
-  //   return <Loading />;
-  // }
-  // return <>
-  //   <h2>모델 목록</h2>
-  //   <FormRow>
-  //     <Label>검색할 내용</Label>
-  //     <input type="text" value={searchKeyword} onChange={({ target: { value } }) => setSearchKeyword(value)}></input>
-  //   </FormRow>
-  //   <FormRow>
-  //     <button style={{ marginRight: '10px' }} type="button" onClick={() => fetch(searchKeyword, null)}>이름 검색</button>
-  //     <button type="button" onClick={() => fetch(null, searchKeyword)}>태그 검색</button>
-  //   </FormRow>
-  //   {assets.map(a => renderAsset(a))}
-  //   <p>{elapsed ? '목록 조회에 ' + elapsed + ' ms 소요되었습니다.' : null}</p>
-  // </>;
 }
 
 export default observer(AssetListPage);
