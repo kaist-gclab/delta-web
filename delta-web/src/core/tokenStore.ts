363165fbee54bbc6acc554ca73743280b0a223a6
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 15:48:52 +0900

    TokenStore 개발

---
+++ delta-web/src/core/tokenStore.ts
@@ -0,0 +1,35 @@
+import { makeAutoObservable } from 'mobx';
+
+class TokenStore {
+    private BrowserTokenStoreKey = 'r29ys6vgnz9tv9vfhszh';
+    private cached: string | null = null;
+    public errorMessage: string | null = null;
+
+    constructor() {
+        makeAutoObservable(this);
+
+        this.updateCache();
+        window.addEventListener('storage', () => { this.updateCache(); });
+    }
+
+    updateCache() {
+        this.cached = window.localStorage.getItem(this.BrowserTokenStoreKey);
+    }
+
+    setToken(token: string): void {
+        window.localStorage.setItem(this.BrowserTokenStoreKey, token);
+        this.errorMessage = null;
+        this.cached = token;
+    }
+
+    clear(): void {
+        window.localStorage.removeItem(this.BrowserTokenStoreKey);
+        this.cached = null;
+    }
+
+    get token(): string | null {
+        return this.cached;
+    }
+}
+
+export default new TokenStore();
