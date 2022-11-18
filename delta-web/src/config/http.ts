1a401806efa972af1b8e8e0a65108ccabb45e542
Hyeonguk Ryu <ryu@hyeonguk.com>
2022-11-18 15:58:46 +0900

    TypeScript InitialToken 구현 제거

--- delta-web/src/config/http.ts
+++ delta-web/src/config/http.ts
@@ -1,3 +1,2 @@
 export const ApiBase: string = import.meta.env.VITE_API_BASE;
 export const AuthBase: string = import.meta.env.VITE_AUTH_BASE;
-export const InitialToken: string = import.meta.env.VITE_INITIAL_TOKEN;
