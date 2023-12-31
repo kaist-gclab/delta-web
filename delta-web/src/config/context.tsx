e5fe607abd2022b386420893b0c7dcf6ffcbf1df
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 13:36:09 +0900

    EncryptionKeyContext 제거

--- delta-web/src/config/context.tsx
+++ delta-web/src/config/context.tsx
@@ -11,7 +11,6 @@ const wrappers: ((app: React.FC) => React.FC)[] = [];
 
 export const AuthContext = addStore(AuthStore);
 export const AssetContext = addStore(AssetStore);
-export const EncryptionKeyContext = addStore(EncryptionKeyStore);
 export const JobContext = addStore(JobStore);
 export const JobTypeContext = addStore(JobTypeStore);
 export const MonitoringContext = addStore(MonitoringStore);
