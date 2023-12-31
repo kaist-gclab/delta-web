ee9cf1a5620e9ed1e7f9600579c733023e9fb522
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 14:21:38 +0900

    신규 처리기 노드 모니터링 API 연결

--- delta-web/src/monitoring/ProcessorNodeMonitorPage.tsx
+++ delta-web/src/monitoring/ProcessorNodeMonitorPage.tsx
@@ -1,16 +1,16 @@
 import { HTMLTable } from '@blueprintjs/core';
 import { observer } from 'mobx-react-lite';
-import { useContext, useEffect } from 'react';
-import { JobContext } from '../config/context';
 import Container from '../core/Container';
-import { Loading } from '../core/NonIdealStates';
+import { ErrorMessage, Loading } from '../core/NonIdealStates';
 import PageHeader from '../core/PageHeader';
+import { Monitoring } from '../api';
+import dayjs from 'dayjs';
 
 function ProcessorNodeMonitorPage() {
-  const store = useContext(JobContext);
-  useEffect(() => { store.fetchAll(); }, [store]);
-
-  const data = store.jobs;
+  const { error, data } = Monitoring.useSWRGetProcessorNodeEvents();
+  if (error) {
+    return <ErrorMessage message="처리기 노드 모니터링 정보를 불러오는 중 오류가 발생했습니다." />;
+  }
   if (!data) {
     return <Loading />;
   }
@@ -26,9 +26,9 @@ function ProcessorNodeMonitorPage() {
           </tr>
         </thead>
         <tbody>
-          {data.map((e) => <tr key={e.id.toString()}>
-            <td>{e.createdAt.toISOString()}</td>
-            <td>{e.jobArguments}</td>
+          {data.map((e) => <tr key={e.time.toString()}>
+            <td>{dayjs(e.time).toISOString()}</td>
+            <td>{e.content}</td>
           </tr>)}
         </tbody>
       </HTMLTable>
