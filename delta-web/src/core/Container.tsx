094f0f2c47db1cc096048d4556053190bea412b0
Hyeonguk Ryu <ryu@hyeonguk.com>
2023-12-31 12:43:51 +0900

    페이지별로 조정된 Container를 적용할 수 있도록 개선

--- delta-web/src/core/Container.tsx
+++ delta-web/src/core/Container.tsx
@@ -1,10 +1,31 @@
 import styled from '@emotion/styled';
 
-const Container = styled.div`
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 20px;
-  padding-bottom: 40px;
+const DefaultContainer = styled.div`
+padding-left: 20px;
+padding-right: 20px;
+padding-top: 20px;
+padding-bottom: 40px;
 `;
+
+function Container(props: {
+  reducedTopPadding?: boolean;
+  noTopPadding?: boolean;
+  children: React.ReactNode;
+  id?: string;
+}) {
+  if (props.noTopPadding) {
+    return <DefaultContainer id={props.id} style={{ paddingTop: 0 }}>
+      {props.children}
+    </DefaultContainer>;
+  }
+  if (props.reducedTopPadding) {
+    return <DefaultContainer id={props.id} style={{ paddingTop: '5px' }}>
+      {props.children}
+    </DefaultContainer>;
+  }
+  return <DefaultContainer id={props.id}>
+    {props.children}
+  </DefaultContainer>;
+}
 
 export default Container;
