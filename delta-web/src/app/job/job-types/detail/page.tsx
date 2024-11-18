810517f8c0d63ed02a3ed293a1b7f4b760b41994
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 00:58:00 +0900

    job/job-types/detail

---
+++ delta-web/src/app/job/job-types/detail/page.tsx
@@ -0,0 +1,59 @@
+import { Button, ControlGroup, InputGroup } from '@blueprintjs/core';
+import { FormEvent, use, useState } from 'react';
+import styled from '@emotion/styled';
+import { ErrorMessage, Loading } from '@/core/NonIdealStates';
+import Container from '@/core/Container';
+import PageHeader from '@/core/PageHeader';
+import { JobTypes } from '@/api/client';
+import { useRouter } from 'next/router';
+
+const QueryButton = styled(Button)`
+padding-left: 20px;
+padding-right: 20px;
+`;
+
+const Message = styled.div`
+margin: 20px 0;
+`;
+
+function JobTypeDetailPage(props: { params: Promise<{ id?: string }> }) {
+  const params = use(props.params);
+  const router = useRouter();
+  const [id, setId] = useState('');
+  const {data, error} = JobTypes.useSWRGetJobTypes();
+  if (error) {
+    return <ErrorMessage message="작업 유형 상세 조회 중 오류가 발생했습니다." />;
+  }
+  if (!data) {
+    return <Loading />;
+  }
+
+  const render = () => {
+    const e = data.find((e) => e.id.toString() === params.id);
+    if (!e) {
+      return <ErrorMessage message="작업 유형 상세 조회 중 오류가 발생했습니다." />;
+    }
+    return <ul>
+      <li>번호: {e.id.toString()}</li>
+      <li>키: {e.key}</li>
+      <li>이름: {e.name}</li>
+    </ul>
+  };
+
+  const goDetailPage = () => router.push(`/job/job-types/detail/${id}`);
+
+  return <Container reducedTopPadding>
+    <PageHeader>작업 유형 상세 조회</PageHeader>
+    <ControlGroup>
+      <InputGroup type="text" leftIcon="numbered-list"
+        placeholder="번호" value={id}
+        onChange={(e: FormEvent<HTMLInputElement>) => {
+          setId(e.currentTarget.value);
+        }} />
+      <QueryButton onClick={goDetailPage}>조회</QueryButton>
+    </ControlGroup>
+    {!params.id ? <Message>번호로 조회하십시오.</Message> : render()}
+  </Container>;
+}
+
+export default JobTypeDetailPage;
