21db6d99830c22cdcafb326bdd18b8321ff1a960
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 00:11:50 +0900

    아이콘 문제 해결

--- delta-web/src/app/start/page.tsx
+++ delta-web/src/app/start/page.tsx
@@ -33,35 +33,34 @@ function StartLabel({ children }: { children: ReactNode }) {
 const IconSize = 40;
 
 function StartPage() {
-  const svgProps = { style: { display: 'inline' } };
   return <Container reducedTopPadding>
     <PageHeader>시작</PageHeader>
     <h2>도움말</h2>
     <StartButton to="/help">
-      <Book size={IconSize} svgProps={svgProps} />
+      <Book size={IconSize} />
       <StartLabel>도움말</StartLabel>
     </StartButton>
     <h2>자주 사용하는 기능</h2>
     <div>
       <StartButton to="/asset/assets/add">
-        <CloudUpload size={IconSize} svgProps={svgProps} />
+        <CloudUpload size={IconSize} />
         <StartLabel>애셋 추가</StartLabel>
       </StartButton>
       <StartButton to="/asset/assets/list">
-        <Cloud size={IconSize} svgProps={svgProps} />
+        <Cloud size={IconSize} />
         <StartLabel>애셋 목록</StartLabel>
       </StartButton>
       <StartButton to="/job/jobs/add">
-        <ThDerived size={IconSize} svgProps={svgProps} />
+        <ThDerived size={IconSize} />
         <StartLabel>작업 추가</StartLabel>
       </StartButton>
       <StartButton to="/job/jobs/list">
-        <Th size={IconSize} svgProps={svgProps} />
+        <Th size={IconSize} />
         <StartLabel>작업 목록</StartLabel>
       </StartButton>
       <br />
       <StartButton to="/monitoring/dashboard">
-        <Dashboard size={IconSize} svgProps={svgProps} />
+        <Dashboard size={IconSize} />
         <StartLabel>대시보드</StartLabel>
       </StartButton>
     </div>
