34916a40b651eb5a56f6b9e29e383f001153e958
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 20:29:03 +0900

    날짜 유틸리티 업데이트

--- delta-web/src/core/datetime.ts
+++ delta-web/src/core/datetime.ts
@@ -1,17 +1,31 @@
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+dayjs.extend(utc);
 
-export function localDate(value: string): string {
+export function localDate(value: string | Dayjs): string {
     return dayjs(value).format('YYYY-MM-DD');
 }
 
-export function localDateTime(value: string): string {
+export function localDateShort(value: string | Dayjs): string {
+    return dayjs(value).format('MM/DD');
+}
+
+export function localDateTime(value: string | Dayjs): string {
     return dayjs(value).format('YYYY-MM-DD HH:mm:ss');
 }
 
-export function localDateTimeShort(value: string): string {
+export function localDateTimeUtc(value: string): string {
+    return dayjs.utc(value).local().format('YYYY-MM-DD HH:mm:ss');
+}
+
+export function localDateTimeShort(value: string | Dayjs): string {
     return dayjs(value).format('MM-DD HH:mm:ss');
 }
 
-export function localDateTimeMicro(value: string): string {
+export function localDateTimeMicro(value: string | Dayjs): string {
     return dayjs(value).format('YYYY-MM-DD HH:mm:ss.SSS');
+}
+
+export function localDayShort(value: string | Dayjs): string {
+    return '일월화수목금토'[dayjs(value).get('day')];
 }
