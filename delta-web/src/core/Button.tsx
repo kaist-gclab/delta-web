f25bd796c415d1582b3b79147db28056a20e10ab
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 21:12:27 +0900

    작은 버튼 지원

--- delta-web/src/core/Button.tsx
+++ delta-web/src/core/Button.tsx
@@ -5,15 +5,17 @@ export function Button(props: {
   children: ReactNode,
   role?: 'delete' | 'submit',
   disabled?: boolean,
+  small?: boolean,
 } & ({} | { onClick: () => void } | { href: string })) {
   const { onClick, className } = useClickOrHref(props);
   const intentClassNames = props.role === 'delete' ?
     'bg-red-500 hover:bg-red-600' :
     'bg-blue-500 hover:bg-blue-600';
+  const sizeClassNames = props.small ? 'text-sm px-3 py-1' : 'px-5 py-3';
 
   return <button type={props.role === 'submit' ? 'submit' : 'button'}
     onClick={onClick} disabled={props.disabled}
-    className={`rounded-md px-5 py-3 text-white disabled:opacity-50 disabled:cursor-not-allowed ${intentClassNames} ${className}`}>
+    className={`rounded-md text-white disabled:opacity-50 disabled:cursor-not-allowed ${sizeClassNames} ${intentClassNames} ${className}`}>
     {props.children}
   </button>;
 }
