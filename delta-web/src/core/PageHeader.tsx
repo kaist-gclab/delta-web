502333bb1554c394dabc1e281412c7ffc17d0839
Hyeonguk Ryu <ryu@hyeonguk.com>
2024-11-19 17:21:31 +0900

    사용자 인터페이스 개선

--- delta-web/src/core/PageHeader.tsx
+++ delta-web/src/core/PageHeader.tsx
@@ -1,7 +1,9 @@
 import { ReactNode } from 'react';
 
 function PageHeader({ children }: { children: ReactNode }) {
-  return <h1>{children}</h1>;
+  return <h1 className="text-2xl font-semibold my-3">
+    {children}
+  </h1>;
 }
 
 export default PageHeader;
